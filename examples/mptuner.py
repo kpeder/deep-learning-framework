57161b6f2c8b25d9ac35b8313a2b1c791900e8f1
Kevin Pedersen <kpedersen@pythian.com>
2024-11-12 16:09:21 -0400

    examples, hypermodel class

---
+++ examples/mptuner.py
@@ -0,0 +1,167 @@
+from contextlib2 import nullcontext
+from deeplearning.utils.config import Config
+from deeplearning.utils.logger import getContextLogger
+from deeplearning.models.seq_conv_2d import SequentialConv2DTunable
+
+import argparse
+import datetime
+import keras_tuner as tuner  # type: ignore
+import logging
+import multiprocessing as mp
+import numpy  # type: ignore
+import os
+import sys
+
+
+'''
+Set up the Python Logger using the configuration class defaults.
+'''
+handler: logging.Handler
+logger: logging.Logger = logging.getLogger(__name__)
+
+conf = Config()
+conf.configure(config=None)
+conf.configure(config={"multiprocessing": {"enabled": True}})  # merge updates
+
+try:
+    formatter = logging.Formatter(conf.configuration["logging"]["format"])
+
+    if conf.configuration["logging"]["type"] == 'stream':
+        handler = logging.StreamHandler()
+        handler.setStream(getattr(sys, conf.configuration["logging"]["path"]))
+
+    if conf.configuration["logging"]["type"] == 'file':
+        logdate = datetime.datetime.now()
+        handler = logging.FileHandler(f'{os.environ["PWD"]}/log/{logdate.strftime("%Y%m%d")}_template.log')
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    if hasattr(logging, conf.configuration["logging"]["level"].upper()):
+        logger.setLevel(getattr(logging, conf.configuration["logging"]["level"].upper()))
+        logger.warning(f'Loglevel has been set to {logger.getEffectiveLevel()} for log {__name__}.')
+
+except Exception as e:
+    raise e
+
+'''
+Configure argument parsing, for convenience.
+'''
+parser = argparse.ArgumentParser()
+
+''' Optional backend override.'''
+parser.add_argument('--keras-backend-override', action='store', dest='keras_backend_override')
+
+args = parser.parse_args()
+
+'''
+Configure and import Keras.
+'''
+os.environ["KERAS_BACKEND"] = (args.keras_backend_override or conf.configuration["keras"]["backend"])
+logger.info(f'Configuring Keras backend as "{os.environ["KERAS_BACKEND"]}".')
+
+import keras  # type: ignore # noqa: E402
+
+
+logger.info(f'Using keras version {keras.__version__}.')
+
+
+def project_worker(project: str):
+    ''' Define the worker function.'''
+    try:
+        with getContextLogger(level=logging.INFO, name=project) as plogger:
+
+            (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+            plogger.info('Loaded the mnist dataset to training and test tensors.')
+
+            x_train = x_train.astype('float32') / 255
+            x_test = x_test.astype('float32') / 255
+            plogger.info('Normalized tensor data to scale [0,1].')
+
+            x_train = numpy.expand_dims(x_train, -1)
+            x_test = numpy.expand_dims(x_test, -1)
+            plogger.info('Expanded data shape to add color channel.')
+
+            plogger.info(f'Train dataframe shape (images, pwidth, pheight, channels): {x_train.shape}')
+            plogger.info(f'Test dataframe shape (images, pwidth, pheight, channels): {x_test.shape}')
+
+            ''' Initialize a tunable Hypermodel.'''
+            with SequentialConv2DTunable(x_train.shape[1:],
+                                         num_classes=10,
+                                         metrics=[keras.metrics.SparseCategoricalAccuracy(name='sparse_categorical_accuracy')],
+                                         verbose=2) as tunable:
+
+                plogger.info(f'Created tunable Hypermodel {tunable.name}.')
+
+                ''' Build our default model, for a sanity check.'''
+                hp = tuner.HyperParameters()
+                plogger.info('Created Hyperparameters class.')
+
+                try:
+                    model = tunable.build(hp)
+                    plogger.info('Built model from default parameters (sanity test).')
+                except Exception as e:
+                    plogger.exception(e)
+                    raise e
+
+                model.summary()
+
+                try:
+                    hp.Fixed('epochs', 1)
+                    tunable.fit(hp, model, x_train, y_train, callbacks=None)
+                    plogger.info('Fit model with one epoch from default parameters (sanity test).')
+                except Exception as e:
+                    plogger.exception(e)
+                    raise e
+
+                ''' Configure callbacks.'''
+                try:
+                    callbacks: keras.callbacks.CallbackList = [
+                        keras.callbacks.EarlyStopping(monitor='sparse_categorical_accuracy',
+                                                      mode='max',
+                                                      patience=2)]
+                    plogger.info(f'Configured callback list with {len(callbacks)} callbacks.')
+                except Exception as e:
+                    plogger.exception(e)
+                    raise e
+
+                try:
+                    tune = tuner.RandomSearch(objective=tuner.Objective('sparse_categorical_accuracy', direction='max'),
+                                              max_trials=4,
+                                              hypermodel=tunable,
+                                              directory="tune",
+                                              project_name=project.strip('__'),
+                                              overwrite=True)
+                    plogger.info(f'Configured {type(tune).__name__} tuner with {type(tunable).__name__} hypermodel.')
+                except Exception as e:
+                    plogger.exception(e)
+                    raise e
+
+                plogger.info(f'Running a parameter space search on the {type(tunable).__name__} hypermodel.')
+                tune.search(x_train=x_train, y_train=y_train, callbacks=callbacks)
+
+                plogger.info(f'Finished parameter space search on the {type(tunable).__name__} hypermodel.')
+
+                return project, tune.get_best_hyperparameters()[0]
+
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+def project_callback(results: list[tuner.Tuner]):
+    try:
+        for t in results:
+            logger.info(f'Project {t[0].project_name} returned best parameters {t[1].values}.')
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+''' Use a worker pool with async map and callback.'''
+with mp.Pool(conf.configuration["multiprocessing"]["workers"]) if conf.configuration["multiprocessing"]["enabled"] else nullcontext() as mpp:
+
+    if mpp is not None:
+
+        results = mpp.map_async(project_worker, ['__par0__', '__par1__'], callback=project_callback)
+        results.wait()
--- src/deeplearning/models/seq_conv_2d.py
+++ src/deeplearning/models/seq_conv_2d.py
@@ -2,6 +2,7 @@ from contextlib2 import AbstractContextManager
 from typing import Literal
 
 import keras  # type: ignore
+import keras_tuner as tuner  # type: ignore
 
 
 class SequentialConv2D(keras.Sequential, AbstractContextManager):
@@ -22,6 +23,87 @@ class SequentialConv2D(keras.Sequential, AbstractContextManager):
             keras.layers.GlobalAveragePooling2D(),
             keras.layers.Dropout(0.5),
             keras.layers.Dense(num_classes, activation="softmax")])
+
+    def __enter__(self):
+        '''
+        Context Manager entry method.
+        '''
+        return self
+
+    def __exit__(self, *args) -> Literal[False]:
+        '''
+        Context Manager exit method.
+        '''
+        return False
+
+
+class SequentialConv2DTunable(tuner.HyperModel, AbstractContextManager):
+    '''
+    A tunable Hypermodel using a sequential regularized convolutional neural network.
+    '''
+    def __init__(self, input_shape: tuple, num_classes: int, name: str = 'SeqConv2DTunable', metrics: list = [], verbose: int = 2):
+        '''
+        Initialize the class.
+        '''
+        super().__init__(name, tunable=True)
+
+        self.input_shape: tuple = input_shape
+        self.num_classes: int = num_classes
+        self.metrics: list = metrics
+        self.verbose: int = verbose
+
+    def build(self, hp):
+        '''
+        Implement the model with Hyperparameters and return the instance.
+        '''
+        global_average_pool: bool = hp.Boolean('global_average_pool', True)
+        initial_filters: int = hp.Int('initial_filters', 32, 64, step=32)
+        initial_kernel_size: int = hp.Int('initial_kernel_size', 1, 3, step=2)
+        loss_function = keras.losses.SparseCategoricalCrossentropy()
+        optimizer_function = keras.optimizers.Adam(learning_rate=hp.Choice('learning_rate', [1e-2, 1e-3, 1e-4]))
+
+        model = keras.Sequential(name=self.name)
+
+        model.add(keras.layers.Input(shape=self.input_shape)),
+        model.add(keras.layers.Conv2D(initial_filters, kernel_size=(initial_kernel_size, initial_kernel_size), activation='relu')),
+        model.add(keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu')),
+        model.add(keras.layers.MaxPooling2D(pool_size=(2, 2))),
+        model.add(keras.layers.Conv2D(128, kernel_size=(3, 3), activation='relu')),
+        model.add(keras.layers.Conv2D(128, kernel_size=(3, 3), activation='relu')),
+        if global_average_pool:
+            model.add(keras.layers.GlobalAveragePooling2D()),
+        else:
+            model.add(keras.layers.MaxPooling2D(pool_size=(2, 2))),
+            model.add(keras.layers.Flatten()),
+        model.add(keras.layers.Dropout(hp.Float('dropout', 0.3, 0.7, step=0.2, sampling='linear'))),
+        model.add(keras.layers.Dense(self.num_classes, activation='softmax'))
+
+        model.compile(
+            loss=loss_function,
+            metrics=self.metrics,
+            optimizer=optimizer_function
+        )
+
+        return model
+
+    def fit(self,
+            hp,
+            model,
+            x_train,
+            y_train,
+            callbacks: keras.callbacks.CallbackList):
+        '''
+        Train the model with Hyperparameters and return the instance.
+        '''
+        return model.fit(
+            x_train,
+            y_train,
+            callbacks=callbacks,
+            batch_size=hp.Int('batch_size', 32, 256, step=2, sampling='log'),
+            epochs=hp.Fixed('epochs', 8),
+            validation_split=hp.Float('validation_split', 0.1, 0.3, step=0.05),
+            verbose=self.verbose
+        )
 
     def __enter__(self):
         '''

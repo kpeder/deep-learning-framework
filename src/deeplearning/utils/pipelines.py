7f92fedad220fe80733a33f711651655b270b19a
Kevin Pedersen <kpedersen@pythian.com>
2024-11-26 15:20:09 -0400

    add pipeline tests, docstrings

--- src/deeplearning/utils/pipelines.py
+++ src/deeplearning/utils/pipelines.py
@@ -21,6 +21,19 @@ def create_csv_pipeline(pipeline_name: str,
                         metadata_path: Optional[str] = None):
     '''
     Builds a TFX pipeline from imported CSV data.
+
+    Args:
+        pipeline_name: A string specifying the pipeline's name.
+        pipeline_root: A string specifying the pipeline's root path.
+        data_root: A string specifying the path of the CSV data source.
+        enable_cache: A bool indicating whether pipeline caching should be enabled.
+        metadata_path: A optional string specifying the location of the metadata store, relative to the pipeline root.
+
+    Returns:
+        A tfx.dsl.Pipeline, configured for tfx.components.CSVExampleGen input.
+
+    Raises:
+        e (Exception): Any unhandled exception, as necessary.
     '''
     components: list = []
 
@@ -49,6 +62,19 @@ def create_tfr_pipeline(pipeline_name: str,
                         metadata_path: Optional[str] = None):
     '''
     Builds a TFX pipeline from imported TF Records.
+
+    Args:
+        pipeline_name: A string specifying the pipeline's name.
+        pipeline_root: A string specifying the pipeline's root path.
+        data_root: A string specifying the path of the serialized (TF Record) data source.
+        enable_cache: A bool indicating whether pipeline caching should be enabled.
+        metadata_path: A optional string specifying the location of the metadata store, relative to the pipeline root.
+
+    Returns:
+        A tfx.dsl.Pipeline, configured for tfx.components.ImportExampleGen input.
+
+    Raises:
+        e (Exception): Any unhandled exception, as necessary.
     '''
     components: list = []
 
@@ -77,6 +103,19 @@ def _create_pipeline(pipeline_name: str,
                      metadata_connection_config: metadata_store_pb2.ConnectionConfig = None):
     '''
     Build a TFX pipeline.
+
+    Args:
+        pipeline_name: A string specifying the pipeline's name.
+        pipeline_root: A string specifying the pipeline's root path.
+        components: A list of TFX components to add to the pipeline, can be empty.
+        enable_cache: A bool indicating whether pipeline caching should be enabled.
+        metadata_connection_config: A ConnectionConfig object specifying the metadata store connection.
+
+    Returns:
+        A tfx.dsl.Pipeline, configured with the components supplied in the components list.
+
+    Raises:
+        e (Exception): Any unhandled exception, as necessary.
     '''
     try:
         return tfx.dsl.Pipeline(
@@ -90,25 +129,60 @@ def _create_pipeline(pipeline_name: str,
 
 
 def _bytes_feature(value):
-    '''Returns a bytes_list from a string / byte.'''
+    '''
+    Creates a tf.train.Feature from a bytes object.
+
+    Args:
+        value: A bytes object.
+
+    Returns:
+        A tf.train.Feature object containing a tf.train.BytesList.
+    '''
     if isinstance(value, type(tf.constant(0))):
         value = value.numpy()
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 
 def _float_feature(value):
-    '''Returns a float_list from a float / double.'''
+    ''' Creates a tf.train.Feature from a float or double.
+
+    Args:
+        value: A number of type float.
+
+    Returns:
+        A tf.train.Feature object containing a tf.train.FloatList.
+    '''
     return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
 
 
 def _int64_feature(value):
-    '''Returns an int64_list from a bool / enum / int / uint.'''
+    '''
+    Returns an tf.train.Feature from a bool, enum, int or uint.
+
+    Args:
+        value: A number of type int.
+
+    Returns:
+        A tf.train.Feature object containing a tf.train.Int64List.
+    '''
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 
 @contextmanager
 def get_tmp_dir(prefix: str, clean: bool = True):
-    ''' Creates a temporary directory and then cleans it up.'''
+    '''
+    Creates a temporary directory and then cleans it up.
+
+    Args:
+        prefix: A string specifying the prefix to use for the temp directory name.
+        clean: A flag to specify whether to clean up the directory on exit.
+
+    Returns:
+        directory: A string specifying the created directory.
+
+    Raises:
+        e (Exception): Any unhandled exception, as necessary.
+    '''
 
     directory = tmp.mkdtemp(prefix=prefix)
     try:
@@ -130,12 +204,30 @@ def get_tmp_dir(prefix: str, clean: bool = True):
 
 
 def serialize_array(array):
+    '''
+    Serialize a raw tensor.
+
+    Args:
+        array: A tensor (numpy.ndarray).
+
+    Returns:
+        array: A serialized array.
+    '''
     array = tf.io.serialize_tensor(array)
     return array
 
 
-def serialize_image_data(image: numpy.array, label: int):
-    ''' Serializes images and labels to TF Record.'''
+def serialize_image_data(image: numpy.array, label: numpy.array):
+    '''
+    Serializes images and labels to tf.train.Example format.
+
+    Args:
+        image: An image tensor, as numpy.array of bytes.
+        label: A categorical label, as a numpy.array of binary byte values.
+
+    Returns:
+        A tf.train.Example object containing a tf.train.Features record.
+    '''
     record = {
         'height': _int64_feature(value=image.shape[0]),
         'width': _int64_feature(value=image.shape[1]),

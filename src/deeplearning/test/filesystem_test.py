14cfd4d7cbed4ca4e4d6dc02fc1cf7d8b7a78b76
Kevin Pedersen <kpedersen@pythian.com>
2024-11-29 15:53:46 -0400

    add data pusher and tests

--- src/deeplearning/test/filesystem_test.py
+++ src/deeplearning/test/filesystem_test.py
@@ -1,4 +1,4 @@
-from deeplearning.utils.filesystem import data_fetcher
+from deeplearning.utils.filesystem import data_fetcher, data_pusher
 
 import logging
 import os
@@ -28,8 +28,42 @@ def test_data_fetcher():
         _data_root = temp_root
 
         try:
-            data_fetcher(name=_pipeline_name, source=_data_source_url, dest=_data_root)
+            location = data_fetcher(name=_pipeline_name, source=_data_source_url, dest=_data_root)
             assert os.path.exists(os.path.join(temp_root, '.'.join((_pipeline_name, _data_source_url.split('.')[-1]))))
+            assert location.startswith(_data_root)
         except Exception as e:
             logger.exception(e)
             raise e
+
+
+def test_data_pusher():
+    '''
+    Function to test the data_pusher function.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Raises:
+        e (Exception): Any unhandled exception, as necessary.
+    '''
+
+    with tempfile.TemporaryDirectory(prefix='snowcrabs-data-src') as temp_source:
+        _pipeline_name = 'snowcrabs'
+        _data_source = '/'.join((temp_source, 'snowcrabbes.txt'))
+
+        _data_file = open(_data_source, 'w')
+        _data_file.write('TEST\n')
+        _data_file.close()
+
+        with tempfile.TemporaryDirectory(prefix='snowcrabs-data-dest') as temp_dest:
+            try:
+                location = data_pusher(name=_pipeline_name, source=_data_source, dest=temp_dest)
+                assert os.path.exists(location)
+                with open(location, 'r') as _dest_file:
+                    assert _dest_file.read() == 'TEST\n'
+            except Exception as e:
+                logger.exception(e)
+                raise e
--- src/deeplearning/utils/filesystem.py
+++ src/deeplearning/utils/filesystem.py
@@ -7,6 +7,7 @@ import urllib.request as geturl
 
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 def data_fetcher(name: str, source: str, dest: str):
@@ -19,7 +20,7 @@ def data_fetcher(name: str, source: str, dest: str):
         dest: A string specifying filesystem or cloud storage destination.
 
     Returns:
-        None
+        destination: A string specifying the location of the data.
 
     Raises:
         e (Exception): Any unhandled exception, as necessary.
@@ -43,8 +44,56 @@ def data_fetcher(name: str, source: str, dest: str):
             blob = bucket.blob(os.path.join('/'.join(dest.split('/')[3:]), _data_filepath.split('/')[-1]))
             blob.upload_from_filename(_data_filepath)
             logger.info(f'Uploaded blob {blob.name} to bucket {bucket.name} from file {_data_filepath}.')
-
+            location = os.path.join('gs://', f'{bucket.name}', f'{blob.name}')
         else:
             if not os.path.exists(dest):
                 os.makedirs(dest)
             os.rename(_data_filepath, '/'.join((dest, _data_filepath.split('/')[-1])))
+            location = '/'.join((dest, _data_filepath.split('/')[-1]))
+
+        return location
+
+
+def data_pusher(name: str, source: str, dest: str):
+    '''
+    Pushes data to pipeline data root. Supports GCS destinations.
+
+    Args:
+        name: A string specifying the pipeline name.
+        source: A string specifying the data source.
+        dest: A string specifying filesystem or cloud storage destination.
+
+    Returns:
+        destination: A string specifying the location of the data.
+
+    Raises:
+        e (Exception): Any unhandled exception, as necessary.
+    '''
+
+    ''' Name the destination file after the pipeline.'''
+    if source.split('/')[-1].lower().split('.')[-1]:
+        _data_filepath = os.path.join('/'.join(source.split('/')[:-1]), '.'.join((name, source.split('/')[-1].lower().split('.')[-1])))
+    else:
+        _data_filepath = os.path.join('/'.join(source.split('/')[:-1]), name)
+
+    if source != _data_filepath:
+        os.rename(source, _data_filepath)
+
+    if dest.startswith('gs://'):
+        cli = storage.Client()
+        bucket = cli.bucket(dest.split('/')[2])
+        blob = bucket.blob(os.path.join('/'.join(dest.split('/')[3:]), _data_filepath.split('/')[-1]))
+        ''' Fix chunk size for large(ish) files.'''
+        if os.stat(_data_filepath).st_size > 20 * 1024 * 1024:
+            blob.chunk_size = 5 * 1024 * 1024
+        logger.info(f'Uploading blob {blob.name} to bucket {bucket.name} from file {_data_filepath}.')
+        blob.upload_from_filename(_data_filepath)
+        logger.info(f'Uploaded blob {blob.name} to bucket {bucket.name} from file {_data_filepath}.')
+        location = os.path.join('gs://', f'{bucket.name}', f'{blob.name}')
+    else:
+        if not os.path.exists(dest):
+            os.makedirs(dest)
+        os.rename(_data_filepath, '/'.join((dest, _data_filepath.split('/')[-1])))
+        location = '/'.join((dest, _data_filepath.split('/')[-1]))
+
+    return location
